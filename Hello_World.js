7342bc363886e6f944b8ceb25c550189a24e6219
janilcort <janilcort@gmail.com>
2025-05-15 20:23:44 -0700

    Html Javascript

---
+++ Hello_World.js
@@ -0,0 +1,7 @@
+
+alert("Hello, World! I am an elert box!");
+function My_first_function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML = result;
+		}
